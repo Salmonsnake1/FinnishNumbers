b0201e9a9cf8f91438a2dbf692d4ad00bc7937ee
Salmonsnake1 <samueldyson1@outlook.com>
2025-04-22 00:25:53 +0300

    Testing github pages

--- script.js
+++ script.js
@@ -301,6 +301,7 @@ function checkAnswer() {
   const answer = document.getElementById("input").value.trim().toLowerCase();
   let correctAns = false;
   let shownAns = "";
+  
   console.log(answer);
   console.log(wordAnswer);
 
